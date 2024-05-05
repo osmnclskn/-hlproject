192af2e50a90955dd4b66d6dfaee2c41457ac4ff
osmnclskn <osmnclskn7@gmail.com>
2024-05-05 20:17:35 +0300

    edited code using 'isort' and 'black'

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
+import streamlit as st
 from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from langchain_community.utilities import SQLDatabase
-from langchain_core.output_parsers import StrOutputParser
-from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-import streamlit as st
+from langchain_openai import ChatOpenAI
 
-def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
+
+def init_database(
+    user: str, password: str, host: str, port: str, database: str
+) -> SQLDatabase:
     """
     Function used to initialize the connection to the database.
 
@@ -24,6 +27,7 @@ def init_database(user: str, password: str, host: str, port: str, database: str)
     """
     db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)
+
 
 def get_sql_chain(db):
     template = """
@@ -50,24 +54,22 @@ def get_sql_chain(db):
     Question: {question}
     SQL Query:
     """
-    
+
     prompt = ChatPromptTemplate.from_template(template)
-  
+
     llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
-  
+
     def get_schema(_):
         return db.get_table_info()
-  
+
     return (
-        RunnablePassthrough.assign(schema=get_schema)
-        | prompt
-        | llm
-        | StrOutputParser()
+        RunnablePassthrough.assign(schema=get_schema) | prompt | llm | StrOutputParser()
     )
-    
+
+
 def get_response(user_query: str, db: SQLDatabase, chat_history: list):
     sql_chain = get_sql_chain(db)
-  
+
     template = """
     You are a analyst at a company. You are interacting with a user who is asking you questions about the company's database.
     Based on the table schema below, question, sql query, and sql response, write a natural language response.
@@ -77,11 +79,11 @@ def get_response(user_query: str, db: SQLDatabase, chat_history: list):
     SQL Query: <SQL>{query}</SQL>
     User question: {question}
     SQL Response: {response}"""
-  
+
     prompt = ChatPromptTemplate.from_template(template)
-  
+
     llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
-  
+
     chain = (
         RunnablePassthrough.assign(query=sql_chain).assign(
             schema=lambda _: db.get_table_info(),
@@ -91,16 +93,20 @@ def get_response(user_query: str, db: SQLDatabase, chat_history: list):
         | llm
         | StrOutputParser()
     )
-  
-    return chain.invoke({
-        "question": user_query,
-        "chat_history": chat_history,
-    })
-    
-  
+
+    return chain.invoke(
+        {
+            "question": user_query,
+            "chat_history": chat_history,
+        }
+    )
+
+
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
-        AIMessage(content="Hello! I'm a SQL assistant. Ask me anything about your database."),
+        AIMessage(
+            content="Hello! I'm a SQL assistant. Ask me anything about your database."
+        ),
     ]
 
 load_dotenv()
@@ -111,14 +117,16 @@ st.title("Chat with MySQL")
 
 with st.sidebar:
     st.subheader("Settings")
-    st.write("This is a simple chat application using MySQL. Connect to the database and start chatting.")
-    
-    st.text_input('Host',value='localhost',key='Host')
-    st.text_input("Port",value='3306',key="Port")
-    st.text_input('User',value='root',key='User')
-    st.text_input('Password',type='password',value='p_ssW0rd',key='Password')
-    st.text_input('Database',value='sakila',key='Database')
-    
+    st.write(
+        "This is a simple chat application using MySQL. Connect to the database and start chatting."
+    )
+
+    st.text_input("Host", value="localhost", key="Host")
+    st.text_input("Port", value="3306", key="Port")
+    st.text_input("User", value="root", key="User")
+    st.text_input("Password", type="password", value="p_ssW0rd", key="Password")
+    st.text_input("Database", value="sakila", key="Database")
+
     if st.button("Connect"):
         with st.spinner("Connecting to database..."):
             db = init_database(
@@ -126,11 +134,11 @@ with st.sidebar:
                 st.session_state["Password"],
                 st.session_state["Host"],
                 st.session_state["Port"],
-                st.session_state["Database"]
+                st.session_state["Database"],
             )
             st.session_state.db = db
             st.success("Connected to database!")
-    
+
 for message in st.session_state.chat_history:
     if isinstance(message, AIMessage):
         with st.chat_message("AI"):
@@ -142,12 +150,14 @@ for message in st.session_state.chat_history:
 user_query = st.chat_input("Type a message...")
 if user_query is not None and user_query.strip() != "":
     st.session_state.chat_history.append(HumanMessage(content=user_query))
-    
+
     with st.chat_message("Human"):
         st.markdown(user_query)
-        
+
     with st.chat_message("AI"):
-        response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
+        response = get_response(
+            user_query, st.session_state.db, st.session_state.chat_history
+        )
         st.markdown(response)
-        
+
     st.session_state.chat_history.append(AIMessage(content=response))
